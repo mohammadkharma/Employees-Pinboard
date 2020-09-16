a2fa933415d520e913c912a18d3b4df66b54ecb9
MYK <mohammad.kh.8688@gmail.com>
2020-09-17 00:15:06 +0200

    created the router with the main components routes and imported them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+
+import Navbar from "./components/navbar.component";
+import PinsList from "./components/pins-list.component";
+import EditPin from "./components/edit-pin.component";
+import CreatePin from "./components/create-pin.component";
+import CreateEmployee from "./components/create-employee.component";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Employees Pinboard</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={PinsList} />
+      <Route path="/edit/:id" exact component={EditPin} />
+      <Route path="/create" exact component={CreatePin} />
+      <Route path="/employee" exact component={CreateEmployee} />
+    </Router>
   );
 }
 
