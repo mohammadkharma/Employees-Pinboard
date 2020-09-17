bcaddbb0910923501943a5d2a9e4637d3c257122
MYK <mohammad.kh.8688@gmail.com>
2020-09-17 21:23:23 +0200

    handled the frontend of create-pin component

--- src/components/create-pin.component.js
+++ src/components/create-pin.component.js
@@ -1,11 +1,115 @@
 import React, { Component } from "react";
+// import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 class CreatePin extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+
+    this.onChangeEmployeeName = this.onChangeEmployeeName.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      employeeName: "",
+      description: "",
+      date: new Date(),
+      employees: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      employees: ["test employee"],
+      employeeName: "test employee",
+    });
+  }
+
+  onChangeEmployeeName(e) {
+    this.setState({
+      employeeName: e.target.value,
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value,
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const pin = {
+      employeeName: this.state.employeeName,
+      description: this.state.description,
+      date: this.state.date,
+    };
+
+    console.log(pin);
+
+    window.location = "/";
+  }
+
   render() {
     return (
       <div>
-        <p>Create Pin Component</p>
+        <h3>Create New Pin Log</h3>
+
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>EmployeeName: </label>
+            <select
+              ref="employeeInput"
+              required
+              className="form-control"
+              value={this.state.employeeName}
+              onChange={this.onChangeEmployeeName}
+            >
+              {this.state.employees.map(function (employee) {
+                return (
+                  <option key={employee} value={employee}>
+                    {employee}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Description: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create Pin Log"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
       </div>
     );
   }
