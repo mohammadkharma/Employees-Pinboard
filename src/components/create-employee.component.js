2fd127e069d6a7af50a9078374faa6312cf0b892
MYK <mohammad.kh.8688@gmail.com>
2020-09-17 22:00:02 +0200

    handled the frontend of create employee component

--- src/components/create-employee.component.js
+++ src/components/create-employee.component.js
@@ -1,11 +1,61 @@
 import React, { Component } from "react";
 
 class CreateEmployee extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+
+    this.onChangeEmployeeName = this.onChangeEmployeeName.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      employeeName: "",
+    };
+  }
+
+  onChangeEmployeeName(e) {
+    this.setState({
+      employeeName: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const employee = {
+      employeeName: this.state.employeeName,
+    };
+
+    console.log(employee);
+
+    this.setState({
+      employeeName: "",
+    });
+  }
   render() {
     return (
       <div>
-        <p>Create Employee Component</p>
+        <h3>Create New Employee</h3>
+
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>EmployeeName: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.employeeName}
+              onChange={this.onChangeEmployeeName}
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create Employee"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
       </div>
     );
   }
