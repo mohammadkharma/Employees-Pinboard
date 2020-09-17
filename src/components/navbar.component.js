c53bd600bb294ca19776e972d842602988324e71
MYK <mohammad.kh.8688@gmail.com>
2020-09-17 21:22:41 +0200

    fixed syntax error

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -12,12 +12,12 @@ class Navbar extends Component {
           <ul className="navbar-nav mr-auto">
             <li className="navbar-item">
               <Link to="/" className="nav-link">
-                Create Pin Log
+                Pins
               </Link>
             </li>
             <li className="navbar-item">
               <Link to="/create" className="nav-link">
-                Create Pin Log
+                Create Pin
               </Link>
             </li>
             <li className="navbar-item">
